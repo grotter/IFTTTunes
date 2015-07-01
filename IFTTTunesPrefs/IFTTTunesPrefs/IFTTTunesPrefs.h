02024be6561a908454622046b44143abebf17dbc
Greg Rotter <grotter@gmail.com>
2015-07-01 09:52:36 -0700

    Basic non-functional IB stuff for an OS X preference pane

---
+++ IFTTTunesPrefs/IFTTTunesPrefs/IFTTTunesPrefs.h
@@ -0,0 +1,15 @@
+//
+//  IFTTTunesPrefs.h
+//  IFTTTunesPrefs
+//
+//  Created by Rotter, Greg on 7/1/15.
+//  Copyright (c) 2015 Greg Rotter. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface IFTTTunesPrefs : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ IFTTTunesPrefs/IFTTTunesPrefs/IFTTTunesPrefs.m
@@ -0,0 +1,17 @@
+//
+//  IFTTTunesPrefs.m
+//  IFTTTunesPrefs
+//
+//  Created by Rotter, Greg on 7/1/15.
+//  Copyright (c) 2015 Greg Rotter. All rights reserved.
+//
+
+#import "IFTTTunesPrefs.h"
+
+@implementation IFTTTunesPrefs
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
