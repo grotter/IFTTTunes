06cdc71ee654d12101d996276f3d472d5580ebdb
Greg Rotter <grotter@gmail.com>
2015-10-21 11:51:45 -0700

    updating deploy script

--- deploy/install.sh
+++ deploy/install.sh
@@ -5,8 +5,8 @@ cd /tmp
 # download binaries to appropriate locations
 curl -o /tmp/IFTTTunes.tar.gz https://raw.githubusercontent.com/grotter/IFTTTunes/master/deploy/IFTTTunes.tar.gz
 tar xvzf /tmp/IFTTTunes.tar.gz
-cp -R /tmp/IFTTTunesPrefs.prefPane ${HOME}/Library/PreferencePanes
-cp -R /tmp/IFTTTunes.app /Applications
+cp -R /tmp/IFTTTunes/IFTTTunesPrefs.prefPane ${HOME}/Library/PreferencePanes
+cp -R /tmp/IFTTTunes/IFTTTunes.app /Applications
 
 # set as hidden startup item
 osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/IFTTTunes.app", hidden:true}'
